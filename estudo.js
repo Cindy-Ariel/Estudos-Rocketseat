3a29958e1c3243fd5f15015eb88c60f558bf8aff
Johnny-Brendow <94567136+Cindy-Ariel@users.noreply.github.com>
2022-01-10 11:23:42 -0300

    Testes de aprendizado
    Conforme fui assistindo as aulas fui fazendo alguns testes.
    Arrow function , Callback function , Manipulando Array ,Operador condicional ternário,  if .. else básico  , Scope, New ,Falsy e truthy..

--- estudo.js
+++ estudo.js
@@ -42,8 +42,8 @@
 // 		return this.name + " está andando";
 // 	};
 // }
-// const Mayk = new Person("Mayk");
-// const Joao = new Person("João");
+// const mayk = new Person("Mayk");
+// const joao = new Person("João");
 
 // console.log(Mayk.walk());
 // console.log(Joao.walk());
@@ -52,18 +52,18 @@
 
 // let techs = ["Python", "js", "java", "C"];
 
-//adicionar um item no fim
+// // adicionar um item no fim
 // techs.push("Java Script");
 
-//adicionar um item no começo
+// //adicionar um item no começo
 // techs.unshift("PHP");
 
-//remover do fim
-//techs.pop();
+// //remover do fim
+// techs.pop();
 
-//pegar somente alguns elementos, posiçao inicial x posição final.
+// //pegar somente alguns elementos, posiçao inicial x posição final.
 
-//console.log(techs.slice(0, 1));
+// console.log(techs.slice(0, 1));
 
 //remover um ou mais itens em qualquer posiçao do array, posição x quantidade,comecando do 0.
 //techs.splice(1, 2);
@@ -103,50 +103,184 @@
 
 //EXERCÍCIO PRATICANDO SOZINHA. (TRANSFORMAR NOTAS ESCOLARES)
 
-let suanota = 61;
+// let suanota = 61;
 
-function resultado() {
-	if (suanota >= 90) {
-		return "A";
-	} else if (suanota >= 80) {
-		return "B";
-	} else if (suanota >= 70) {
-		return "C";
-	} else if (suanota >= 60) {
-		return "D";
-	} else if (suanota <= 59) {
-		return "uma péssima nota";
-	}
-	return "VALOR INCORRETO";
-}
+// function resultado() {
+// 	if (suanota >= 90) {
+// 		return "A";
+// 	} else if (suanota >= 80) {
+// 		return "B";
+// 	} else if (suanota >= 70) {
+// 		return "C";
+// 	} else if (suanota >= 60) {
+// 		return "D";
+// 	} else if (suanota <= 59) {
+// 		return "uma péssima nota";
+// 	}
+// 	return "VALOR INCORRETO";
+// }
 
-console.log(`Sua nota foi ${suanota} e você tirou ${resultado()}.`);
+// console.log(`Sua nota foi ${suanota} e você tirou ${resultado()}.`);
 
 //EXERCÍCIO PRATICANDO SOZINHA. (SISTEMA DE GASTOS FAMILIAR)
 
-let family = {
-	receitas: [1500, 200, 100.5, 300.5],
-	despesas: [200.1, 500, 100.6, 3000],
+// let family = {
+// 	receitas: [1500, 200, 100.5, 300.5],
+// 	despesas: [200.1, 500, 100.6, 3000],
+// };
+
+// function soma(array) {
+// 	let total = 0;
+// 	for (let value of array) {
+// 		total += value;
+// 	}
+// 	return total;
+// }
+
+// function calculo() {
+// 	const calculoreceitas = soma(family.receitas);
+// 	const calculodespesas = soma(family.despesas);
+
+// 	const total = calculoreceitas - calculodespesas;
+// 	const epositivo = total >= 1;
+// 	let balancoteste = "negativo";
+// 	if (epositivo) {
+// 		balancoteste = "positivo";
+// 	}
+// 	console.log(`Seu saldo é ${balancoteste}:${total}`);
+// }
+// calculo();
+
+//EXERCÍCIO PRATICANDO SOZINHA. (SISTEMA DE GASTOS FAMILIAR)
+
+// const empresa = {
+// 	funcionarios: [
+// 		{
+// 			funcionario_1: {
+// 				nome: "João",
+// 				sobrenome: "Paulo",
+// 				profissao: "motorista",
+// 				sexo: "masculino",
+// 			},
+// 		},
+// 		{
+// 			funcionario_2: {
+// 				sexo: "feminino",
+// 				nome: "Ana",
+// 				sobrenome: "Clara",
+// 				profissao: "faxineira",
+// 			},
+// 		},
+// 		{
+// 			funcionario_3: {
+// 				nome: "Pedro",
+// 				sexo: "masculino",
+// 				sobrenome: "Bortollo",
+// 				profissao: "medico",
+// 			},
+// 		},
+// 	],
+// 	nome: "Loop",
+// 	cnpj: "39.636.696/0001-20",
+// };
+
+// function nomecompleto(nome, sobrenome) {
+// 	let nomeesobrenome = nome + sobrenome;
+
+// 	return nomeesobrenome;
+// }
+// nomecompleto(empresa.funcionarios.funcionario_1);
+// nomecompleto();
+
+// function calculo() {
+// 	const calculoreceitas = soma(family.receitas);
+// 	const calculodespesas = soma(family.despesas);
+
+// 	const total = calculoreceitas - calculodespesas;
+// 	const epositivo = total >= 1;
+// 	let balancoteste = "negativo";
+// 	if (epositivo) {
+// 		balancoteste = "positivo";
+// 	}
+// 	console.log(`Seu saldo é ${balancoteste}:${total}`);
+// }
+// calculo();
+
+//Scope
+
+// const banana = "catura";
+// const leite = "integral";
+
+// function fazer_vitamina_de_banana(leite) {
+// 	const banana = "prata";
+
+// 	return ` Vitamina de banana feita com banana ${banana} e leite ${leite}.`;
+// }
+// console.log(
+// 	`Os ingredientes que foram feito a vitamina foram banana ${banana} e leite ${leite}.`
+// );
+
+// console.log(fazer_vitamina_de_banana(leite));
+
+// console.log(
+// 	`Os ingredientes que foram feito a vitamina foram banana ${banana} e leite ${leite}.`
+// );
+
+//
+
+// Callback Function
+
+// const callback = (number2) => {
+// 	console.log("Sou o numero um e estou antes da callback.");
+
+// 	number2();
+
+// 	console.log("Sou o numero tres e estou depois da callback.");
+// };
+
+// callback(() => {
+// 	console.log("Sou o numero dois e estou dentro da callback.");
+// });
+
+// console.log(Number("9") + 5);
+
+//Transformar uma cadeia de caracteres em elementos de um array
+
+//  let word = "paralelepipedo";
+//  console.log(Array.from(word));
+
+// let nome = "cindy";
+// console.log(Array.from(nome));
+
+//Criar Array com construtor
+
+// let myArray = new Array("a", "b", "c");
+// console.log(myArray);
+
+// New
+
+// let endereço = new String("Rua flor,55 caiçara");
+// endereço.complemento = "casa";
+
+// console.log(endereço);
+
+//Falsy e truthy
+
+let soumaior = () => {
+	idade = 50;
+	const resultado = idade >= 18;
 };
+const idade = 2;
+console.log(idade);
+soumaior();
+console.log(idade);
 
-function sum(array) {
-	let total = 0;
-	for (let value of array) {
-		total += value;
-	}
-	return total;
-}
+// if e else
 
-function calculo() {
-	const calculoreceitas = sum(family.receitas);
-	const calculodespesas = sum(family.despesas);
+// let age = 2;
 
-	const total = calculoreceitas - calculodespesas;
-	const itsOk = total >= 0;
-	let balancoteste = "negativo";
-	if (itsOk) {
-		balancoteste = "positivo";
-	}
-	console.log(`Seu saldo é ${balancoteste}:${total}`);
-}
-calculo();
+// if (age > 18) {
+// 	console.log("Você é maior de age");
+// } else {
+// 	console.log("Você é menor de age");
+// }
